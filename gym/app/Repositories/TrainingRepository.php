79bf5af9e4e42d31d1cdfd27d971d55eef0a04d2
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-15 10:51:47 +0200

    Created TrainingRepository.php to handle training related database operations

---
+++ gym/app/Repositories/TrainingRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Training;
+use App\Services\WeekCalculator;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class TrainingRepository
+{
+    public function __construct(protected WeekCalculator $weekCalculator)
+    {}
+    public function getTrainingsByWeek(Request $request) {
+        $week = $request->query('week');
+        Log::info($week);
+
+        if ($week == null)
+        {
+            $trainings = Training::with(['trainingMethod', 'trainer', 'trainees' => function ($query) {
+                $query->select('users.id');
+            }])
+                ->withCount('trainees')
+                ->whereNotNull('trainer_id')
+                ->where('start', '>=', Carbon::now()->setTimezone('GMT+2'))
+                ->orderBy('start')
+                ->get();
+
+            //return response()->json($trainings);
+        }
+
+        else
+        {
+//            $currentDate = Carbon::now();
+//            $startOfWeek = $currentDate->addWeeks((int)$week)->startOfWeek()->startOfDay()->toDateTimeString();
+//            $endOfWeek = $currentDate->endOfWeek()->endOfDay()->toDateTimeString();
+
+            $period = $this->weekCalculator->calculateWeek((int)$week);
+
+            $trainings = Training::whereBetween('start', $period)
+                ->with(['trainingMethod', 'trainer', 'trainees' => function ($query) {
+                    $query->select('users.id');
+                }])
+                ->withCount('trainees')
+                ->whereNotNull('trainer_id')
+                ->where('start', '>=', Carbon::now()->setTimezone('GMT+2'))
+                ->orderBy('start')
+                ->get();
+        }
+
+        return $trainings;
+    }
+}
