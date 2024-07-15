2fcee30c3a603566aebc97c70b8bc0c39934fb50
BaloghBalazs91 <blay1991@gmail.com>
2024-07-15 12:02:03 +0200

    Home picture url modified,

--- gym/resources/views/home.blade.php
+++ gym/resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div align="center">
-    <img src="{{ asset('storage/' . 'images/home-picture.jpg') }}" alt="Gym Picture" class="w-75" >
+    <img src="{{ asset('storage/images/home-picture.jpg') }}" alt="Gym Picture" class="w-75" >
     </div>
     <br>
     <h1 align="center">Welcome to our modern and well-equipped gym's website! We are proud to present a platform that caters to all fitness enthusiasts!</h1><br>
